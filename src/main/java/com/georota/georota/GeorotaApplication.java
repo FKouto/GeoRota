72dd7c6c56c6d4895b058e9a8f6b4b785b291ac1
Felipe Couto <coutodev@icloud.com>
2024-10-25 19:17:26 -0300

    Initial commit (Initial Spring Boot) :tada:

---
+++ src/main/java/com/georota/georota/GeorotaApplication.java
@@ -0,0 +1,13 @@
+package com.georota.georota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorotaApplication.class, args);
+	}
+
+}
