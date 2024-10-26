bb68958d9baa2fad41679f80dd75f5fef7d2ef3b
Felipe Couto <coutodev@icloud.com>
2024-10-26 18:12:09 -0300

    Limpeza da addPonto, addConexao para commit :pencil:

--- src/main/java/com/georota/georota/GeorotaApplication.java
+++ src/main/java/com/georota/georota/GeorotaApplication.java
@@ -9,9 +9,9 @@ public class GeorotaApplication {
     public static void main(String[] args) {
 //		SpringApplication.run(GeorotaApplication.class, args);
 		Cidade osasco = new Cidade();
-		osasco.addPonto("City Bussocaba - Parque Municipal");
-		osasco.addPonto("City Bussocaba - Escola Municipal");
-		osasco.addConexao("City Bussocaba - Parque Municipal", "City Bussocaba - Escola Municipal", 2.5);
+		osasco.addPonto("");
+		osasco.addPonto("");
+		osasco.addConexao("", "");
 		osasco.exibirCidade();
     }
 
