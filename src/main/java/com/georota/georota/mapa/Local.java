a37e4547054254cc75fd17a9260210d44bd3fb1f
Felipe Couto <coutodev@icloud.com>
2024-11-16 15:33:32 -0300

    Add classe Local, representa um Ponto de Interesse com um endereço :construction:

---
+++ src/main/java/com/georota/georota/mapa/Local.java
@@ -0,0 +1,31 @@
+package com.georota.georota.mapa;
+// Importação das anotações do Lombok para geração automática de getters e setters
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Local extends Ponto {
+    private double distancia;
+    private List<Ponto> conexao;
+
+    // Construtor que inicializa o campo "nomePonto" e "logradouro" através do construtor da classe pai
+    public Local(String nomePonto, String logradouro) {
+        super(logradouro, nomePonto);
+        this.conexao = new ArrayList<>();
+    }
+
+    public Local(String nomePonto, String logradouro, double distancia) {
+        super(logradouro, nomePonto);
+        this.distancia = distancia;
+    }
+
+    // Método que adiciona um ponto à lista de conexões ("conexao")
+    public void criarConexaoLocal(Ponto ponto) {
+        this.conexao.add(ponto);
+    }
+}
+
